a12ece4452dcbb42a6c3c6f13372dad2ce447fb7
sr229 <aguilarralph13@gmail.com>
2018-10-03 16:22:22 +0800

    fix mod folder not being created

--- travis/inject_compile_target.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# AliceOS Specific Shell Script
-
-description_template="REPLACE"
-
-  sed -i "s/build.package(build.directory_name + \"CoreFiles\",'zip',build.name,description='REPLACE')/build.package(build.directory_name + \"$DIST_TARGET\",'zip',build.name,description='$DIST_TARGET-compatible module')/g" mod/game/options.rpy;
-  cat mod/game/options.rpy | grep build.package;
-  
